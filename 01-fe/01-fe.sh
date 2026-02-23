8bcfc33b1900282de81d53326690fe39fa09cd40
Oranjeviy-Kisel <39113496+Oranjeviy-Kisel@users.noreply.github.com>
2026-02-23 11:33:26 +0000

    Update 01-fe.sh

--- 01-fe/01-fe.sh
+++ 01-fe/01-fe.sh
@@ -7,36 +7,5 @@ echo 'Hello from host @01-fe, beginning to install'
 # проверяем актуальность apt update
 apt update
 
-#echo $?
+echo $?
 
-# Устанавливается веб-сервер NginX
-
-apt install -y nginx
-
-# проверить предыдущий exit-code предыдущей выполнившейся команды
-# в случае успеха устанавливаем модуль мониторинга
-
-if [[ "$?" -eq 0 ]]; 
-  then
-    apt install prometheus-node-exporter -y
-fi
-
-
-#устанавливается prometheus-node-exporter
-# apt install prometheus-node-exporter
-
-# ------------------------------------------------------ 
-
-rm -rf /etc/nginx/sites-available/default
-cp ./default /etc/nginx/sites-available/default
-chmod 755 /etc/nginx/sites-available/default
-chmod 755 /etc/nginx/sites-enabled/default
-
-#/etc/nginx/nginx.conf
-
-
-# рестартуем nginx
-service nginx restart
-
-
-#проверяем, что все работает на запущенной машине пингом
